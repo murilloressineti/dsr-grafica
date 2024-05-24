e61370ee6277f8c1c901629eb544c745f2663daa
Murillo Silva <murillobr22@gmail.com>
2024-05-24 14:05:38 -0300

    update menu

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.getElementById('menu-toggle');
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
     const menu = document.getElementById('menu');
 
-    menuToggle.addEventListener('click', function () {
+    menuToggle.addEventListener('click', (event) => {
+        menuToggle.classList.add('hidden');
         menu.classList.toggle('active');
+        event.stopPropagation();
+    });
+
+    document.addEventListener('click', (event) => {
+        if (!menu.contains(event.target)) {
+            menu.classList.remove('active');
+            menuToggle.classList.remove('hidden');
+        }
+    });
+
+    menu.addEventListener('click', (event) => {
+        if (event.target.tagName === 'A') {
+            event.preventDefault();
+            const targetId = event.target.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+            if (targetElement) {
+                const offsetTop = targetElement.offsetTop;
+                window.scrollTo({
+                    top: offsetTop,
+                    behavior: 'smooth'
+                });
+            } else {
+                console.error('Elemento alvo não encontrado:', targetId);
+            }
+        }
     });
 });
